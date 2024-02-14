d04cea71cc5aded6ae465cb69b90aba85bc7dda1
Antonin <antoninlefevre45@icloud.com>
2024-02-14 20:03:09 +0100

    inference

--- Transfer_Learning_using_EfficientNet_PyTorch/src/inference.py
+++ Transfer_Learning_using_EfficientNet_PyTorch/src/inference.py
@@ -120,4 +120,4 @@ def start_inference(diffusion, model_name, show_results=False) -> [float, float,
 
 
 if __name__ == '__main__':
-    start_inference(diffusion='perona-malik', model_name='efficient_net_b1', show_results=False)
+    start_inference(diffusion='coherence-enhancing', model_name='efficient_net_b1', show_results=False)
